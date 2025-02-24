092a65f98c0ad4d79fc4f3f1676797328a8ed5d2
andrew-r-lawler <andrew.r.lawler@gmail.com>
2025-02-24 14:35:49 -0600

    main.go returns random hex string for jwt secrets

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"io"
+)
+
+// GenerateRandomHexString generates a random hex string of specified length
+func GenerateRandomHexString(length int) (string, error) {
+	// Calculate the number of bytes needed to achieve the desired hex string length
+	byteLength := length / 2
+
+	// Create a byte slice to store the random bytes
+	randomBytes := make([]byte, byteLength)
+
+	// Read random bytes from the crypto/rand source
+	_, err := io.ReadFull(rand.Reader, randomBytes)
+	if err != nil {
+		return "", err
+	}
+
+	// Encode the random bytes to a hexadecimal string
+	hexString := hex.EncodeToString(randomBytes)
+
+	return hexString, nil
+}
+
+func main() {
+	hexString, err := GenerateRandomHexString(64)
+	if err != nil {
+		panic(err)
+	}
+	println(hexString)
+}
